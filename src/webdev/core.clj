fd4d11b9f56ce30aab32aa76da7464b6cbc7bd53
Brandon Orther <brandon.orther@gmail.com>
2015-04-24 17:16:35 -0700

    Added /calc

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn about [req]
   {:status 200
@@ -19,10 +20,37 @@
    :body "Hello, World!"
    :headers {}})
 
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+   {:status 200
+    :body (str "Yo! " name "!!")}))
+
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [a (Integer. (get-in req [:route-params :a]))
+        b (Integer. (get-in req [:route-params :b]))
+        op (get-in req [:route-params :op])
+        f (get ops op)]
+    (if f
+     {:status 200
+      :body (str (f a b))
+      :headers {}}
+     {:status 404
+      :body (str "Unknown operator: " op)
+      :headers {}})))
+
 (defroutes app
   (GET "/" [] greet)
   (GET "/about" [] about)
+  (GET "/calc/:a/:op/:b" [] calc)
   (GET "/goodbye" [] goodbye)
+  (GET "/request" [] handle-dump)
+  (GET "/yo/:name" [] yo)
   (not-found "Page not found."))
 
 (defn -main [port]
