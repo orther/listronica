fec739bc2b48be8eca785d85699f1e3bc9d5fd19
Brandon Orther <brandon.orther@gmail.com>
2015-04-27 12:39:51 -0700

    Created and added listronica app to project.

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.2"]
-                 [compojure "1.3.3"]]
+                 [compojure "1.3.3"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc4"]
+                 [hiccup "1.0.5"]]
 
   :min-lein-version "2.0.0"
 
--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,9 +1,22 @@
 (ns webdev.core
+  (:require [webdev.item.model :as items]
+            [webdev.item.handler :refer [handle-index-items
+                                         handle-create-item
+                                         handle-delete-item
+                                         handle-update-item]])
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
-            [compojure.core :refer [defroutes GET]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
             [compojure.route :refer [not-found]]
             [ring.handler.dump :refer [handle-dump]]))
+
+(def db
+  (or
+   (System/getenv "DATABASE_URL")
+   "jdbc:postgresql://localhost/webdev"))
 
 (defn about [req]
   {:status 200
@@ -44,17 +57,53 @@
       :body (str "Unknown operator: " op)
       :headers {}})))
 
-(defroutes app
+(defroutes routes
   (GET "/" [] greet)
   (GET "/about" [] about)
   (GET "/calc/:a/:op/:b" [] calc)
   (GET "/goodbye" [] goodbye)
-  (GET "/request" [] handle-dump)
+  (ANY "/request" [] handle-dump)
   (GET "/yo/:name" [] yo)
+
+  (GET "/items" [] handle-index-items)
+  (POST "/items" [] handle-create-item)
+  (DELETE "/items/:item-id" [] handle-delete-item)
+  (PUT "/items/:item-id" [] handle-update-item)
+
   (not-found "Page not found."))
 
+(defn wrap-db [hdlr]
+  (fn [req]
+    (hdlr (assoc req :webdev/db db))))
+
+(defn wrap-server [hdlr]
+  (fn [req]
+    (assoc-in (hdlr req) [:headers "Server"] "Listronica 9001 BEO")))
+
+(def sim-methods {"PUT" :put
+                  "DELETE" :delete})
+
+(defn wrap-simulated-methods [hdlr]
+  (fn [req]
+    (if-let [method (and (= :post (:request-method req))
+                         (sim-methods (get-in req [:params "_method"])))]
+      (hdlr (assoc req :request-method method))
+      (hdlr req))))
+
+(def app
+  (wrap-server
+   (wrap-file-info
+    (wrap-resource
+     (wrap-db
+      (wrap-params
+       (wrap-simulated-methods
+        routes)))
+     "static"))))
+
 (defn -main [port]
+  (items/create-table db)
   (jetty/run-jetty app                 {:port (Integer. port)})) 
 
 (defn -dev-main [port]
+  (items/create-table db)
   (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
